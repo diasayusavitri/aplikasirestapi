aeff081d1cee1a04b6bfcff822c3b716f21a1019
Diasayu <diasayusavitri07@outlook.com>
2020-04-24 23:49:33 +0700

    berhasil menampilkan semua data level

--- controller.js
+++ controller.js
@@ -7,6 +7,30 @@ exports.index = function(req,res){
     response.ok("Aplikasi REST API ku berjalan",res)
 };
 
+//menampilkan semua data level
+exports.tampilsemualevel = function(req,res){
+    connection.query('SELECT * FROM t_level', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+//menampilkan semua data level berdasarkan id
+exports.tampilberdasarkanidlevel = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM t_level WHERE id_level = ?', [id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else {
+                response.ok(rows, res);
+            }
+        });
+};
+  
 //menampilkan semua data montir 
 exports.tampilsemuamontir = function(req,res){
     connection.query('SELECT * FROM t_montir', function(error, rows, fields){
@@ -19,9 +43,81 @@ exports.tampilsemuamontir = function(req,res){
 };
 
 //menampilkan semua data montir berdasarkan id
-exports.tampilberdasarkanid = function(req,res){
+exports.tampilberdasarkanidmontir = function(req,res){
     let id = req.params.id;
     connection.query('SELECT * FROM t_montir WHERE id_montir = ?', [id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else {
+                response.ok(rows, res);
+            }
+        });
+};
+
+//menampilkan semua data servis
+exports.tampilsemuaservis = function(req,res){
+    connection.query('SELECT * FROM t_servis', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+//menampilkan semua data servis berdasarkan id
+exports.tampilberdasarkanidservis = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM t_servis WHERE id_servis = ?', [id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else {
+                response.ok(rows, res);
+            }
+        });
+};
+
+//menampilkan semua data sparepart
+exports.tampilsemuasparepart = function(req,res){
+    connection.query('SELECT * FROM t_sparepart', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+//menampilkan semua data sparepart berdasarkan id
+exports.tampilberdasarkanidsparepart = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM t_sparepart WHERE id_sparepart = ?', [id],
+        function(error, rows, fields){
+            if(error){
+                console.log(error);
+            }else {
+                response.ok(rows, res);
+            }
+        });
+};
+
+//menampilkan semua data user
+exports.tampilsemuauser = function(req,res){
+    connection.query('SELECT * FROM t_user', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
+
+//menampilkan semua data user berdasarkan id
+exports.tampilberdasarkaniduser = function(req,res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM t_user WHERE id_user = ?', [id],
         function(error, rows, fields){
             if(error){
                 console.log(error);
--- routes.js
+++ routes.js
@@ -6,9 +6,33 @@ module.exports = function(app) {
     app.route('/')
         .get(jsonku.index);
 
-    app.route('/tampil')
+    app.route('/tampillevel')
+        .get(jsonku.tampilsemualevel);
+    
+    app.route('/tampillevel/:id')
+        .get(jsonku.tampilberdasarkanidlevel);
+
+    app.route('/tampilmontir')
         .get(jsonku.tampilsemuamontir);
     
-    app.route('/tampil/:id')
-        .get(jsonku.tampilberdasarkanid);
+    app.route('/tampilmontir/:id')
+        .get(jsonku.tampilberdasarkanidmontir);
+    
+    app.route('/tampilservis')
+        .get(jsonku.tampilsemuaservis);
+    
+    app.route('/tampilservis/:id')
+        .get(jsonku.tampilberdasarkanidservis);
+
+    app.route('/tampilsparepart')
+        .get(jsonku.tampilsemuasparepart);
+    
+    app.route('/tampilsparepart/:id')
+        .get(jsonku.tampilberdasarkanidsparepart);
+
+    app.route('/tampiluser')
+        .get(jsonku.tampilsemuauser);
+    
+    app.route('/tampiluser/:id')
+        .get(jsonku.tampilberdasarkaniduser);
 }
